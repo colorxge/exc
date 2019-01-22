dec5975e1bbbc3a45fd8fbd4a20e3cb49d6022aa
肖兴 <34230856+colorxge@users.noreply.github.com>
2019-01-22 10:39:22 +0800

    Update log_analysis.py

--- log_analysis.py
+++ log_analysis.py
@@ -1,5 +1,9 @@
 import re
 import datetime
+import random
+import time
+from queue import Queue
+import threading
 
 
 logexample = '''183.69.210.164 - - [07/Apr/2017:09:32:46 +0800] "GET /app/template/default//style/css.css HTTP/1.1" 200 8803 "http://job.magedu.com/index.php?m=login" "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.221 Safari/537.36 SE 2.X MetaSr 1.0"'''
@@ -26,7 +30,7 @@ def extract(line):
         info = {k: ops.get(k, lambda x: x)(v) for k, v in matcher.groupdict().items()}
     return info
 
-print(extract(logexample))
+# print(extract(logexample))
 
 # 数据载入
 
@@ -44,4 +48,43 @@ def load(path:str):
                 yield fields
             else:
                 continue
+
+
+# 时间窗口
+def source():
+    yield {'datetime':datetime.datetime.now(), 'data':random.randint(1, 100)}
+    time.sleep(1)
+
+s = source()
+items = [next(source()) for _ in range(3)]
+print(items)
+
+# 处理函数
+def handle(data):
+    vals = [x['data'] for x in data]
+    return sum(vals) / len(vals)
+
+# print("{:.2f}".format(handle(items)))
+
+def window(src, handle, width, interval):
+    start = datetime.datetime.strptime('20190101 00:00:00', '%Y%m%d %H:%M:%S')
+    current = datetime.datetime.strptime('20190101 00:01:00', '%Y%m%d %H:%M:%S')
+    buffer = []
+    delta = datetime.timedelta(width - interval)
+    while True:
+        data = next(src)
+        if data:
+            buffer.append(data)
+            current = data['datetime']
+
+        if (current - start).total_seconds() >= interval:
+            ret = handle(buffer)
+            print("{:.2f}".format(ret))
+            start = current
+
+            buffer = [x for x in buffer if x['datetime'] > current - delta]
+
+
+# window(source(), handle, 10, 5)
+
 
