a2ae5267df4bf05123443b32415dcfc875069bff
肖兴 <34230856+colorxge@users.noreply.github.com>
2019-04-30 16:03:18 +0800

    Create SocketServer_code.py

---
+++ socket/SocketServer_code.py
@@ -0,0 +1,56 @@
+from socketserver import ThreadingTCPServer, BaseRequestHandler
+import threading
+import sys
+import logging
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(thread)d %(message)s")
+
+
+class ChatHandler(BaseRequestHandler):
+    clients = {}
+
+    def setup(self):
+        super().setup()
+        self.event = threading.Event()  # 初始工作
+        self.clients[self.client_address] = self.request
+
+    def finish(self):
+        super().finish()    # 清理工作
+        self.clients.pop(self.client_address)
+        self.event.set()
+
+    def handle(self):
+        super().handle()
+
+        while not self.event.is_set():
+            data = self.request.recv(1024).decode()
+            print(data, '~~~~~~') # 增加
+            if not data or data == 'quit':
+                break
+            msg = "{} {}".format(self.client_address, data).encode()
+            logging.info(msg)
+            for c in self.clients.values():
+                print('+++++++')
+                c.send(msg)
+        print('End')
+
+
+addr = ('0.0.0.0', 9999)
+server = ThreadingTCPServer(addr, ChatHandler)
+
+server_thread = threading.Thread(target=server.serve_forever, name='ChatServer', daemon=True)
+server_thread.start()
+
+try:
+    while True:
+        cmd = input(">>>")
+        if cmd.strip() == 'quit':
+            server.shutdown()
+            break
+        print(threading.enumerate())
+except Exception as e:
+    print(e)
+except KeyboardInterrupt:
+    pass
+finally:
+    print('Exit')
+    sys.exit(0)
