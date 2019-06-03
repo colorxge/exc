3bf79283156f4a1c3656910a334caca837853768
xing.xiao <xiao_1069@163.com>
2019-06-03 10:32:41 +0800

    adjust the format

--- project_format/send_mail.py
+++ project_format/send_mail.py
@@ -1,24 +1,25 @@
 # smtplib 发送邮件流程
 # 连接到smtp服务器
+import smtplib
 smtp = smtplib.SMTP('smtp.qq.com', 25)
 
 # 发送SMTP的'hello'信息
-smtp.ehlo()
-Out[24]: 
-(250,
- b'smtp.qq.com\nPIPELINING\nSIZE 73400320\nSTARTTLS\nAUTH LOGIN PLAIN\nAUTH=LOGIN\nMAILCOMPRESS\n8BITMIME')
+# smtp.ehlo()
+# Out[24]:
+# (250,
+#  b'smtp.qq.com\nPIPELINING\nSIZE 73400320\nSTARTTLS\nAUTH LOGIN PLAIN\nAUTH=LOGIN\nMAILCOMPRESS\n8BITMIME')
 
 # 将当前会话加密
 smtp.starttls()
-Out[25]: (220, b'Ready to start TLS')
+# Out[25]: (220, b'Ready to start TLS')
 
 # 登陆到SMTP服务器（密码为SMTP服务第三方授权码）
 smtp.login('4****1@qq.com', 'b****h')
-Out[26]: (235, b'Authentication successful')
+# Out[26]: (235, b'Authentication successful')
 
 # 发送邮件
 smtp.sendmail('4xxx2@qq.com', 'xixxx@163.com', 'Subject:this is test\nthis is a')
-Out[27]: {}
+# Out[27]: {}
 
 # 关闭连接
 smtp.quit()
